8a2551fc07d739d1be0e708d1c25f8de0d4fd376
mim1q <mim1q.dev@gmail.com>
2022-10-19 21:04:59 +0200

    Renaming Villagers and Wandering Traders implemented

---
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/VillagerEntityMixin.java
@@ -0,0 +1,28 @@
+package com.github.mim1q.convenientnametags.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin extends MerchantEntity {
+  private VillagerEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+    super(entityType, world);
+  }
+
+  @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
+  protected void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+    if (player.getMainHandStack().isOf(Items.NAME_TAG)) {
+      cir.setReturnValue(ActionResult.PASS);
+    }
+  }
+}
---
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/WanderingTraderEntityMixin.java
@@ -0,0 +1,28 @@
+package com.github.mim1q.convenientnametags.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WanderingTraderEntity.class)
+public abstract class WanderingTraderEntityMixin extends MerchantEntity {
+  private WanderingTraderEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+    super(entityType, world);
+  }
+
+  @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
+  protected void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+    if (player.getMainHandStack().isOf(Items.NAME_TAG)) {
+      cir.setReturnValue(ActionResult.PASS);
+    }
+  }
+}
