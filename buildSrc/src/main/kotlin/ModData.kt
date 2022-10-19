01206449b2d43b4871721e13cfa6a2dc5610b4ff
mim1q <mim1q.dev@gmail.com>
2022-10-19 21:03:14 +0200

    Setup goals for 0.3.0

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,7 +1,7 @@
 object ModData {
   const val group = "com.github.mim1q"
   const val id = "convenientnametags"
-  const val version = "0.2.1"
+  const val version = "0.3.0"
   const val versionType = "release"
   val mcVersions = listOf("1.19", "1.19.1", "1.19.2")
   val dependencies = listOf("fabric-api")
---
+++ changelogs/convenientnametags_0.3.0.md
@@ -0,0 +1,7 @@
+# 0.3.1
+
+## New features
+
+- Added a crafting recipe for Name Tags
+- Made renaming Villagers possible
+- Name Tags now drop from previously named mobs
