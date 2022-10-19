7ebcaaf011429bda1ca69dcd4ebb4abafa06721d
mim1q <mim1q.dev@gmail.com>
2022-10-19 22:50:39 +0200

    Name tags drop from already named mobs when naming them again

--- src/main/java/com/github/mim1q/convenientnametags/mixin/NameTagItemMixin.java
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/NameTagItemMixin.java
@@ -1,15 +1,22 @@
 package com.github.mim1q.convenientnametags.mixin;
 
+import com.github.mim1q.convenientnametags.interfaces.RemovableNameTag;
 import com.github.mim1q.convenientnametags.screen.RenameNameTagScreen;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.NameTagItem;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(net.minecraft.item.NameTagItem.class)
+@Mixin(NameTagItem.class)
 public abstract class NameTagItemMixin extends Item {
   public NameTagItemMixin(Settings settings) {
     super(settings);
@@ -21,10 +28,20 @@ public abstract class NameTagItemMixin extends Item {
     if (hand == Hand.OFF_HAND) {
       return TypedActionResult.pass(itemStack);
     }
-
     if (world.isClient()) {
       RenameNameTagScreen.open(user, itemStack);
     }
     return TypedActionResult.success(itemStack);
+  }
+
+  @Inject(method = "useOnEntity(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+  public void useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+    if (stack.getName().equals(entity.getCustomName())) {
+      cir.setReturnValue(ActionResult.PASS);
+      return;
+    }
+    if (!user.world.isClient() && stack.hasCustomName() && entity.isAlive()) {
+      ((RemovableNameTag) entity).removeNameAndNameTag();
+    }
   }
 }
