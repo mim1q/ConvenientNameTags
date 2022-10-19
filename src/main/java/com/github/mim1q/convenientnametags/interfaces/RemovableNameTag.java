8d18e3f7f45cf4284b17313e7cb70401e9f448a7
mim1q <mim1q.dev@gmail.com>
2022-10-19 22:00:32 +0200

    Name tagged entities drop their name tags on death

---
+++ src/main/java/com/github/mim1q/convenientnametags/interfaces/RemovableNameTag.java
@@ -0,0 +1,5 @@
+package com.github.mim1q.convenientnametags.interfaces;
+
+public interface RemovableNameTag {
+  void removeNameAndNameTag();
+}
---
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/LivingEntityMixin.java
@@ -0,0 +1,36 @@
+package com.github.mim1q.convenientnametags.mixin;
+
+import com.github.mim1q.convenientnametags.interfaces.RemovableNameTag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity implements RemovableNameTag {
+  private LivingEntityMixin(EntityType<?> type, World world) {
+    super(type, world);
+  }
+
+  @Inject(method = "dropInventory()V", at = @At("HEAD"))
+  protected void dropInventory(CallbackInfo info) {
+    if (this.hasCustomName()) {
+      this.removeNameAndNameTag();
+    }
+  }
+
+  public void removeNameAndNameTag() {
+    ItemStack nameTagItemStack = new ItemStack(Items.NAME_TAG);
+    nameTagItemStack.setCustomName(this.getCustomName());
+    ItemEntity item = new ItemEntity(this.world, this.getX(), this.getY(), this.getZ(), nameTagItemStack);
+    this.world.spawnEntity(item);
+    this.setCustomName(null);
+  }
+}
