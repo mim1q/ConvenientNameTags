bc31f2dbc30d08b1b607800d0aaee94f2ad22030
mim1q <mim1q.dev@gmail.com>
2022-10-20 14:44:40 +0200

    Interaction mixin applied to more entities

--- src/main/java/com/github/mim1q/convenientnametags/mixin/ArmorStandMixin.java
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/ArmorStandMixin.java
@@ -2,36 +2,27 @@ package com.github.mim1q.convenientnametags.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.*;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({
-  AbstractDonkeyEntity.class,
-  HorseEntity.class,
-  MobEntity.class,
-  MooshroomEntity.class,
-  SheepEntity.class,
-  SnowGolemEntity.class,
-  VillagerEntity.class,
-  WanderingTraderEntity.class
-})
-public abstract class CancelInteractMobMixin extends Entity {
-  private CancelInteractMobMixin(EntityType<? extends MobEntity> entityType, World world) {
-    super(entityType, world);
+@Mixin(ArmorStandEntity.class)
+public abstract class ArmorStandMixin extends Entity {
+  public ArmorStandMixin(EntityType<?> type, World world) {
+    super(type, world);
   }
 
-  @Inject(method = "interactMob(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
-  protected void cancelInteractMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+  @Inject(method = "interactAt(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+  protected void cancelInteractAt(PlayerEntity player, Vec3d hitPos, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
     ItemStack stack = player.getStackInHand(hand);
     boolean isNameTagApplicable = stack.isOf(Items.NAME_TAG)
       && stack.hasCustomName()
