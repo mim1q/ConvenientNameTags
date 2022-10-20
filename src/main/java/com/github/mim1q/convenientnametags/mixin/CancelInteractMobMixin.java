8a8e2d06b5a8dde74a5f3b1aeeaa53a8216b6631
mim1q <mim1q.dev@gmail.com>
2022-10-20 12:54:32 +0200

    Generalized villager and wandering trader mixins

--- src/main/java/com/github/mim1q/convenientnametags/mixin/CancelInteractMobMixin.java
+++ src/main/java/com/github/mim1q/convenientnametags/mixin/CancelInteractMobMixin.java
@@ -1,9 +1,11 @@
 package com.github.mim1q.convenientnametags.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -13,15 +15,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(VillagerEntity.class)
-public abstract class VillagerEntityMixin extends MerchantEntity {
-  private VillagerEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+@Mixin({
+  VillagerEntity.class,
+  WanderingTraderEntity.class
+})
+public abstract class CancelInteractMobMixin extends MobEntity {
+  private CancelInteractMobMixin(EntityType<? extends MobEntity> entityType, World world) {
     super(entityType, world);
   }
 
   @Inject(method = "interactMob(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
   protected void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-    if (player.getMainHandStack().isOf(Items.NAME_TAG)) {
+    ItemStack stack = player.getStackInHand(hand);
+    if (stack.isOf(Items.NAME_TAG)
+      || (player.isSneaking() && this.hasCustomName() && stack.isOf(Items.SHEARS))) {
       cir.setReturnValue(ActionResult.PASS);
     }
   }
